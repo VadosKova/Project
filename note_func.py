c649d8bfb657461f9972a65c71c43a839faa3126
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-02-05 15:16:59 +0200

    Added method for creating notes

--- note_func.py
+++ note_func.py
@@ -3,3 +3,10 @@ from note import Note
 class NoteFunc:
     def __init__(self):
         self.notes = []
+
+    def create_note(self):
+        title = input("Title: ")
+        content = input("Content: ")
+        new_note = Note(title, content)
+        self.notes.append(new_note)
+        print("Created")
